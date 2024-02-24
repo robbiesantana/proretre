38922cde455084cd58d63714ee6046aa14410ea4
Robert Santana <roberticaro@icloud.com>
2024-02-24 16:39:20 -0500

    Initial Commit

---
+++ proretre/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  proretre
+//
+//  Created by Island Films on 2/24/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ proretre/proretreApp.swift
@@ -0,0 +1,17 @@
+//
+//  proretreApp.swift
+//  proretre
+//
+//  Created by Island Films on 2/24/24.
+//
+
+import SwiftUI
+
+@main
+struct proretreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
